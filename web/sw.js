3e5a843d723be6eb06e58fec52a44b8ac514b711
Hao Yang <yanghao2022@shanghaitech.edu.cn>
2026-05-26 23:56:47 +0800

    commit

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "ear-training-20260530i";
+const CACHE = "ear-training-20260530j";
 const SHELL = ["index.html", "app.js", "styles.css", "sw.js"];
 
 self.addEventListener("install", (event) => {
